f7cdf3fa64747f218790caa6f5c46d5de7bc35aa
shixingxing <shixingxing@yungengxin.com>
2022-11-09 17:28:11 +0800

    初次提交

---
+++ TypeTextView/src/main/java/com/shixingxing/TypeTextView.kt
@@ -0,0 +1,16 @@
+package com.shixingxing
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+/**
+ * 类描述:打字机效果自定义类
+ * 出处:https://github.com/zmywly8866/TypeTextView
+ */
+class TypeTextView : AppCompatTextView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+}
---
+++ app/src/main/java/com/example/type_text_view/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.type_text_view
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.type_text_view.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+}
